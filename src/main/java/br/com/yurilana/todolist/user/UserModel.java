333b2fb5a7c9ad32e6a5e0df15d7c8fa47bd0158
Yuri Rafael <yurilana1001@gmail.com>
2023-10-13 18:27:24 -0300

    Criacao de tasks e filters

--- src/main/java/br/com/yurilana/todolist/user/UserModel.java
+++ src/main/java/br/com/yurilana/todolist/user/UserModel.java
@@ -1,7 +1,26 @@
 package br.com.yurilana.todolist.user;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import org.hibernate.annotations.CreationTimestamp;
 
+@Data
+@Entity(name="tb_users")
 public class UserModel {
-    public String name;
-    public String username;
-    public String password;
+    
+    @Id
+    @GeneratedValue(generator = "UUID") // gera um ID automatico
+    private UUID id;
+
+    private String name;
+    @Column(unique = true)
+    private String username;
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
